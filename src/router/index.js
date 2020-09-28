aa9ae089ed1a0bca14417dd5bf0dca530aa697d5
Eric Mangpang <airrakedev@gmail.com>
2020-09-28 16:56:59 +0800

    another update on package.json

--- src/router/index.js
+++ src/router/index.js
@@ -9,100 +9,101 @@ import store from 'Store'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'main-dashboard',
-    component: () => import('Views/dashboard')
+   {
+      path: '/',
+      name: 'main-dashboard',
+      component: () => import('Views/dashboard')
 
-  },
-  {
-    path: '*',
-    name: '403',
-    component: () => import('Views/403')
-  },
-  {
-    path: '/admin/login',
-    name: 'AdminLogin',
-    component: () => import('Views/admin/login'),
-    beforeEnter: (to, from, next) => {
-      if (store.getters['Admin/getAdminSessionStatus']) {
-        next({ name: 'AdminDashboard' })
-      } else {
-        next()
+   },
+   {
+      path: '*',
+      name: '403',
+      component: () => import('Views/403')
+   },
+   {
+      path: '/admin/login',
+      name: 'AdminLogin',
+      component: () => import('Views/admin/login'),
+      beforeEnter: (to, from, next) => {
+         if (store.getters['Admin/getAdminSessionStatus']) {
+            next({ name: 'AdminDashboard' })
+         } else {
+            next()
+         }
       }
-    }
-  },
-  {
-    path: '/admin',
-    redirect: 'admin/dashboard',
-    component: () => import('Views/admin'),
-    children: [
-      {
-        path: 'dashboard',
-        name: 'AdminDashboard',
-        component: () => import('Views/admin/dashboard'),
-        meta: {
-          requireAuth: true
-        }
+   },
+   {
+      path: '/admin',
+      redirect: 'admin/dashboard',
+      component: () => import('Views/admin'),
+      children: [
+         {
+            path: 'dashboard',
+            name: 'AdminDashboard',
+            component: () => import('Views/admin/dashboard'),
+            meta: {
+               requireAuth: true
+            }
 
-      },
-      {
-        path: 'movie-list',
-        name: 'AdminMovieList',
-        component: () => import('Views/admin/movieList'),
-        meta: {
-          requireAuth: true
-        }
-      },
-      {
-        path: 'all-clients',
-        name: 'AllClientList',
-        component: () => import('Views/admin/clientList'),
-        meta: {
-          requireAuth: true
-        }
-      },
-      {
-        path: 'admin-profile',
-        name: 'AdminProfile',
-        component: () => import('Views/admin/adminProfile'),
-        meta: {
-          requireAuth: true
-        }
-      }
-    ]
-  }
+         },
+         {
+            path: 'movie-list',
+            name: 'AdminMovieList',
+            component: () => import('Views/admin/movieList'),
+            meta: {
+               requireAuth: true
+            }
+         },
+         {
+            path: 'all-clients',
+            name: 'AllClientList',
+            component: () => import('Views/admin/clientList'),
+            meta: {
+               requireAuth: true
+            }
+         },
+         {
+            path: 'admin-profile',
+            name: 'AdminProfile',
+            component: () => import('Views/admin/adminProfile'),
+            meta: {
+               requireAuth: true
+            }
+         }
+      ]
+   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+   mode: 'history',
+   base: (process.env.NODE_ENV != 'development') ? process.env.VUE_APP_PROD_URL : process.env.VUE_APP_API_URL,
+   //   base: process.env.BASE_URL,
+   routes
 })
 
 router.beforeEach((to, from, next) => {
-  Nprogress.start()
+   Nprogress.start()
 
-  if (to.matched.some(route => route.meta.requireAuth)) {
-    if (store.getters['Admin/getAdminSessionStatus']) {
+   if (to.matched.some(route => route.meta.requireAuth)) {
+      if (store.getters['Admin/getAdminSessionStatus']) {
+         next()
+      } else {
+         next({ name: 'main-dashboard' })
+      }
+   } else {
+      // if (store.getters[ 'Admin/getAdminSessionStatus' ]) {
+      //   next({ name: 'AdminDashboard' })
+      // } else {
+      //   next()
+      // }
       next()
-    } else {
-      next({ name: 'main-dashboard' })
-    }
-  } else {
-    // if (store.getters[ 'Admin/getAdminSessionStatus' ]) {
-    //   next({ name: 'AdminDashboard' })
-    // } else {
-    //   next()
-    // }
-    next()
-  }
+   }
 
 
 })
 
 router.afterEach((to, from) => {
-  Nprogress.done()
+   Nprogress.done()
 
 })
 
