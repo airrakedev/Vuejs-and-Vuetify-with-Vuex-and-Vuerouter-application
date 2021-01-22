f6518d5777ccd8d64ef078817ab55224154d27d0
Eric Mangpang <airrakedev@gmail.com>
2021-01-22 22:48:27 +0800

    Fixed preview movie details.

---
+++ src/components/AppUtilities/AppImage.vue
@@ -0,0 +1,22 @@
+<template>
+	<v-img :src="image" :position="position" aspect-ratio="1" :contain="contain">
+		<slot></slot>
+	</v-img>
+</template>
+
+<script>
+export default {
+	name: "AppImage",
+	props: {
+		image: {
+			type: String
+		},
+		position: {
+			type: String
+		},
+		contain: {
+			type: String
+		}
+	}
+};
+</script>
